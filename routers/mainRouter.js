8ace3955f36139fbaa06691c88036d8699ed5dca
kangsoontan <tankangsoon@gmail.com>
2017-02-20 12:37:30 +0800

    add routers to 3 pages (view, add, my projects)

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -5,4 +5,16 @@ router.get('/', function(req, res) {
   res.render('pages/main');
 });
 
+router.get('/addProject', function(req, res) {
+  res.render('pages/addProject');
+});
+
+router.get('/viewProject', function(req, res) {
+  res.render('pages/viewProject');
+});
+
+router.get('/myProjects', function(req, res) {
+  res.render('pages/myProjects');
+});
+
 module.exports = router;
