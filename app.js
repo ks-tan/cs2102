51b4b113df0511a73920e2c493fdff726577b81c
kangsoontan <tankangsoon@gmail.com>
2017-02-20 12:16:03 +0800

    ejs setup

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ const express = require('express');
 const dbRouter = require('./routers/databaseRouter');
 const constants = require('./constants');
 const bodyParser = require('body-parser');
+const mainRouter = require('./routers/mainRouter');
 
 const app = express();
 app.set('port', constants.PORT_CONNECTION);
+app.set('view engine', 'ejs');
 
-//for retrieving form data
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public')); //place to server static files
+app.use(bodyParser.urlencoded({ extended: true })); //for retrieving form data
 
 //set routers
+app.use('/', mainRouter);
 app.use('/db', dbRouter);
 
 app.listen(app.get('port'), function(){
