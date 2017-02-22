9a8092b513486c019b466107b275b39d8a8e785a
Baron <a0122545@u.nus.edu>
2017-02-22 23:51:03 +0800

    Added new executer and route for adding new project

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -35,8 +35,19 @@ router.get('/my-projects', function(req, res) {
   res.render('pages/myProjects');
 });
 
+/*  ============================================================
+    Create new Account
+    @Params
+    string            : username
+    string            : fullname
+    string            : description           
+    int               : age
+    string            : gender (MALE, FEMALE, OTHER)
+    string            : email
+    string            : country
+    string            : role
+    ============================================================*/
 router.post('/account', function (req, res, next) {
-  
   var username = req.body.username;
   var fullname = req.body.fullname;
   var description = (req.body.description) ? req.body.description : '';
@@ -46,6 +57,36 @@ router.post('/account', function (req, res, next) {
   var country = (req.body.country) ? req.body.country : '';
   var role = req.body.role;
   var promise = QUERY_EXECUTER.addAccount(username, fullname, description, age, gender, email, country, role);
+  promise.then(function() {
+    res.redirect('/');  //redirect back to home
+  });
+});
+
+/*  ============================================================
+    Create (and edit? TBI?) new Project
+    @Params
+    string            : title
+    string            : category
+    string            : image_url           
+    string            : description
+    DATE              : start_date(use new Date())
+    DATE              : end_date
+    DECIMAL           : amount_sought
+    string            : owner_account (must be valid username)
+    ============================================================*/
+router.post('/project', function (req, res, next) {
+  console.log(req.body);
+  var title = req.body.title;
+  var category = req.body.category;
+  var image_url = (req.body.image_url) ? req.body.image_url : '';
+  var description = req.body.description;
+
+  var start_date = req.body.start_date;
+  var end_date = req.body.end_date;
+  var amount_sought = req.body.amount_sought;
+  var owner_account = req.body.owner_account.toLowerCase();
+
+  var promise = QUERY_EXECUTER.addProject(title, category, image_url, description, start_date, end_date, amount_sought, owner_account);
   promise.then(function() {
     res.redirect('/');  //redirect back to home
   });
