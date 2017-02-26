427c1470c32f2e4fae2e6525bc9331cacd755c06
whisp3r1ng <whisp3r1ng7@gmail.com>
2017-02-27 02:26:51 +0800

    Added in some data for account and project. Yet to do funds.

--- database/fixtures/populateAccount.js
+++ database/fixtures/populateAccount.js
@@ -7,11 +7,26 @@ const client = new pg.Client(connectionString);
 client.connect();
 
 const queryStatement = 
-        'INSERT INTO account VALUES(DEFAULT, \'dartteon\', \'channnn\', \'i am a nerd!\', 24, \'MALE\', \'dartteon@gmail.com\', \'Singapore\', \'ADMIN\');'
+        'INSERT INTO account VALUES(DEFAULT, \'dartteon\', \'BARON CHAN\', \'i am a nerd!\', 24, \'MALE\', \'dartteon@gmail.com\', \'Singapore\', \'ADMIN\');'
     +   'INSERT INTO account VALUES(DEFAULT, \'chl92\', \'POH HUI LING\', \'FUND ME PLZ\', 29, \'FEMALE\', \'\', \'Singapore\', \'USER\');'
     +   'INSERT INTO account VALUES(DEFAULT, \'xx00\', \'XIE XIN\', \'GAMES GAMES GAMES\', 17, \'FEMALE\', \'xiexin2011@gmail.com\', \'\', \'USER\');'
-    +   'INSERT INTO account VALUES(DEFAULT, \'aneja92\', \'VARGHESE ANEJA\', \'cool handsome dude\', 25, \'MALE\', \'vargheseaneja1992@msn.com\', \'Singapore\', \'USER\');'
-    +   'INSERT INTO account VALUES(DEFAULT, \'tym89\', \'TAY YONG MING\', \'i have no idea what i am doing\', 28, \'MALE\', \'tayyongming1989@gmail.com\', \'Singapore\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'aneja92\', \'VARGHESE ANEJA\', \'Cool handsome dude :)\', 25, \'MALE\', \'vargheseaneja1992@msn.com\', \'Singapore\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'tym89\', \'TAY YONG MING\', \'I have no idea what i am  actually doing\', 28, \'MALE\', \'tayyongming1989@gmail.com\', \'Singapore\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'jenbeck92\', \'JENNY BECKHAM\', \'Hi there! I really really love Geography!\', 25, \'FEMALE\', \'jennybeckham1992@gmail.com\', \'Singapore\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'lyj29\', \'LEE YI JIA\', \'Voluptate consequatur aut et facilis perspiciatis.\', 24, \'FEMALE\', \'leeyijia1989@gmail.com\', \'Singapore\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'hillary33\', \'Virgie Leffler\', \'Ratione ut non aut distinctio et. Qui consectetur cum omnis voluptates officia omnis sint.\', 33, \'FEMALE\', \'mertz.macy@example.org\', \'Morocco\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'jacobs.donnie\', \'Wilhelmine Treutel\', \'Soluta ullam eum veritatis laudantium eum repudiandae cum.\', 41, \'MALE\', \'marquardt.gilbert@example.org\', \'Sweden\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'hnader\', \'Delaney Hoeger\', \'Est eaque sit magnam est. Voluptas et nesciunt totam dolor.\', 37, \'MALE\', \'rhoppe@example.net\', \'Austria\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'yesenia.turcotte\', \'Damien Davis\', \'Amet ea in velit ipsam architecto ducimus ducimus quas.\', 19, \'FEMALE\', \'maybell.skiles@example.org\', \'Martinique\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'bailey.anika\', \'Enrico Tromp\', \'Dignissimos quibusdam facilis error ut accusantium ipsa dolorem et.\', 45, \'FEMALE\', \'clarissa87@example.com\', \'Mayotte\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'grace.predovic\', \'Willa Gulgowski V\', \'Iure voluptatum est praesentium dolorum minima et nihil omnis.\', 39, \'MALE\', \'giuseppe74@example.net\', \'Greece\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'torrance48\', \'Bell Trantow\', \'Dignissimos quibusdam facilis error ut accusantium ipsa dolorem et.\', 55, \'MALE\', \'gerda90@example.org\', \'Niger\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'koch.vincenzo\', \'Julius Gottlieb\', \'Velit cumque consequatur iste provident enim cumque.\', 45, \'MALE\', \'vjohns@example.org\', \'Hungary\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'lakin.roscoe\', \'Amira Beahan\', \'Dolores quo aut ducimus ad quia doloremque eos non.\', 29, \'FEMALE\', \'fidel.walter@example.org\', \'Korea\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'leuschke.rylan\', \'Gayle Lindgren\', \'Error quia saepe temporibus veniam voluptatem.\', 53, \'MALE\', \'andrew96@example.org\', \'Swaziland\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'marjolaine.huels\', \'Violette Vandervort\', \'Magni nihil temporibus id et.\', 27, \'FEMALE\', \'bmuller@example.net\', \'Spain\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'alfonzo.kutch\', \'Gracie Cole\', \'Ut non id laborum voluptatem officia commodi non.\', 59, \'FEMALE\', \'janick61@example.org\', \'Finland\', \'USER\');'
+    +   'INSERT INTO account VALUES(DEFAULT, \'kuphal.burley\', \'Janet Klein\', \'Alias perspiciatis dolores repudiandae.\', 50, \'FEMALE\', \'leda20@example.org\', \'Kyrgyz Republic\', \'USER\');'
     ;
 
 const query1 = client.query(
--- database/fixtures/populateProject.js
+++ database/fixtures/populateProject.js
@@ -7,8 +7,18 @@ const client = new pg.Client(connectionString);
 client.connect();
 
 const queryStatement = 
-        'INSERT INTO project VALUES(DEFAULT, \'Journey to the Moon\', \'Art\', NULL, \'Remake of the first ever movie\', \'2017-02-20\', \'2018-02-20\', \'1000.00\', 1);'
-    +   'INSERT INTO project VALUES(DEFAULT, \'By Gamers, for Gamers\', \'Games\', NULL, \'OVERTHROW VALVE!!!\', \'2017-12-15\', \'2019-12-15\', \'100000.00\', 4);'
+        'INSERT INTO project VALUES(DEFAULT, \'Journey to the Moon\', \'Art\', NULL, \'Remake of the first ever movie\', \'2017-02-20\', \'2018-02-20\', 1000.00, 1);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'By Gamers, for Gamers\', \'Games\', NULL, \'OVERTHROW VALVE!!!\', \'2017-12-15\', \'2019-12-15\', 100000.00, 1);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Overthrow3\', \'Games\', NULL, \'New ideas for a new Overthrow game!\', \'2016-12-16\', \'2018-02-08\', 816520, 4);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'M.C. Escher\', \'Art\', NULL, \'I am inspired by the wonders of nature.\',\'2016-04-26\', \'2017-04-30\', 964087, 3);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Cooking FTW!\', \'Food\', NULL, \'I would like to be the new Gordan Ramsay! Please support!\', \'2016-09-23\', \'2017-04-06\', 405752, 4);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Basking in the streets\', \'Music\', NULL, \'I am trying to update my youtube every week! Donate for content\', \'2016-03-05\', \'2017-08-12\', 625994, 7);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Potato Salad\', \'Others\', NULL, \'If you pledge, you will get some of the potato salad when I make them.\', \'2016-05-09\', \'2017-08-28\', 53980, 8);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Science of Art\', \'Art\', NULL, \'Seeking for funds to make the most epic art piece ever\', \'2016-09-12\', \'2017-05-17\', 984276, 7);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'HeartAttack meal\', \'Food\', NULL, \'Hi! I am currently looking to open a restaurant selling unhealthy food.\', \'2016-09-01\', \'2017-12-20\', 207260, 6);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Dota 3\', \'Games\', NULL, \'OVERTHROW VALVE!!!!\', \'2016-07-19\', \'2017-05-12\', 724627, 5);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Appreciating Latin\', \'Art\', NULL, \'I need funds to continue my studies in Latin and all of its culture!\', \'2016-10-15\', \'2017-12-12\', 701160, 9);'
+    +   'INSERT INTO project VALUES(DEFAULT, \'Rising Star\', \'Music\', NULL, \'Money needed to purchase new equipment for my studio! You will not be disappointed!\', \'2016-12-14\', \'2017-09-19\', 387005, 14);'
     ;
 
 const query1 = client.query(
